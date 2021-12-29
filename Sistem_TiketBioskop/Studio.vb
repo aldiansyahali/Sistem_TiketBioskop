3ad13d653c5fea3993b475e02f888f2817c4a579
alidotcomm <aldiansyahali717@gmail.com>
2021-12-29 09:51:02 +0700

    tambah studio

--- Sistem_TiketBioskop/Studio.vb
+++ Sistem_TiketBioskop/Studio.vb
@@ -8,4 +8,14 @@
         ' Add any initialization after the InitializeComponent() call.
 
     End Sub
+
+    Private Sub TambahStudioToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TambahStudioToolStripMenuItem.Click
+        Dim form_tambah = New TambahStudio
+        form_tambah.Show()
+    End Sub
+
+    Private Sub BtnTambah_Click(sender As Object, e As EventArgs) Handles BtnTambah.Click
+        Dim tambah_studio = New TambahStudio
+        tambah_studio.Show()
+    End Sub
 End Class

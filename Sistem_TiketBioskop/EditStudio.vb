27d1906f3c986aff3fd7abd364cddd28a5c2264b
alidotcomm <aldiansyahali717@gmail.com>
2022-01-05 00:22:16 +0700

    crud studio

--- Sistem_TiketBioskop/EditStudio.vb
+++ Sistem_TiketBioskop/EditStudio.vb
@@ -1,3 +1,21 @@
 ﻿Public Class EditStudio
+    Public Sub New()
 
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        TxtKapasitas.Text = Studio.CStudio.kapasitasProperty
+        TxtHargaKursi.Text = Studio.CStudio.harga_kursiProperty
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Studio.CStudio.kapasitasProperty = Integer.Parse(TxtKapasitas.Text)
+        Studio.CStudio.harga_kursiProperty = Integer.Parse(TxtHargaKursi.Text)
+
+        Studio.CStudio.UpdateDataStudioByIDDatabase(Studio.SelectedTableStudio,
+                                                    Studio.CStudio.kapasitasProperty,
+                                                    Studio.CStudio.harga_kursiProperty)
+        Me.Close()
+    End Sub
 End Class
--- Sistem_TiketBioskop/Studio.vb
+++ Sistem_TiketBioskop/Studio.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Studio
     Public Shared CStudio As CStudio
+    Public Shared SelectedTableStudio As String
     Public Sub New()
 
         ' This call is required by the designer.
@@ -18,6 +19,14 @@
 
     End Sub
 
+    Private Sub DGStudio_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGStudio.CellClick
+        Dim index As Integer = e.RowIndex
+        Dim selectedRow As DataGridViewRow
+        selectedRow = DGStudio.Rows(index)
+
+        SelectedTableStudio = selectedRow.Cells(0).Value
+    End Sub
+
     Private Sub ReloadDataTableDatabase()
         DGStudio.DataSource = CStudio.GetDataStudioDatabase()
     End Sub
@@ -31,5 +40,27 @@
         Dim tambah_studio = New TambahStudio
         tambah_studio.Show()
     End Sub
+
+    Private Sub BtnEdit_Click(sender As Object, e As EventArgs) Handles BtnEdit.Click
+        If SelectedTableStudio IsNot Nothing Then
+            Dim selectedStudio As List(Of String) = CStudio.GetDataStudioByIDDatabase(SelectedTableStudio)
+            CStudio.kapasitasProperty = selectedStudio(1)
+            CStudio.harga_kursiProperty = selectedStudio(2)
+
+            Dim formEdit = New EditStudio()
+            formEdit.Show()
+
+            CStudio.UpdateDataStudioByIDDatabase(SelectedTableStudio,
+                                                               CStudio.kapasitasProperty,
+                                                               CStudio.harga_kursiProperty)
+        Else
+            MessageBox.Show("Tolong pilih salah satu tabel!")
+        End If
+    End Sub
+
+    Private Sub Studio_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        SignIn.Show()
+    End Sub
+
 
 End Class

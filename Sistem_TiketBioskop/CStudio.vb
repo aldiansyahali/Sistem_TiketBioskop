b58943bbed445f13c0e50c83ed0c622f76419650
alidotcomm <aldiansyahali717@gmail.com>
2022-01-05 00:24:51 +0700

    crud studio

---
+++ Sistem_TiketBioskop/CStudio.vb
@@ -0,0 +1,166 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Text
+Public Class CStudio
+    Public Shared dbConn As New MySqlConnection
+    Public Shared sqlCommand As New MySqlCommand
+    Public Shared sqlRead As MySqlDataReader
+    Private sqlQuery As String
+
+    Private server As String = "localhost"
+    Private username As String = "root"
+    Private password As String = ""
+    Private database As String = "tiket_bioskop"
+
+    Private kapasitas As Integer
+    Private harga_kursi As Integer
+
+    Public Property kapasitasProperty() As Integer
+        Get
+            Return kapasitas
+        End Get
+        Set(ByVal value As Integer)
+            kapasitas = value
+        End Set
+    End Property
+
+    Public Property harga_kursiProperty() As Integer
+        Get
+            Return harga_kursi
+        End Get
+        Set(ByVal value As Integer)
+            harga_kursi = value
+        End Set
+    End Property
+
+    Public Function GetDataStudioDatabase() As DataTable
+        Dim result As New DataTable
+
+        dbConn.ConnectionString = "server =" + server + ";" + "user id=" + username + ";" _
+            + "password=" + password + ";" + "database=" + database
+        dbConn.Open()
+        sqlCommand.Connection = dbConn
+        sqlCommand.CommandText = "SELECT id AS 'ID',
+                                  kapasitas as 'kapasitas',
+                                  harga_kursi as 'Harga Kursi'
+                                  FROM studio"
+
+        sqlRead = sqlCommand.ExecuteReader
+
+        result.Load(sqlRead)
+        sqlRead.Close()
+        dbConn.Close()
+        Return result
+    End Function
+
+    Public Function AddDataStudioDatabase(kapasitas As Integer,
+                                           harga_kursi As Integer)
+        dbConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" _
+            + "password=" + password + ";" + "database = " + database
+        Try
+            dbConn.Open()
+            sqlCommand.Connection = dbConn
+            sqlQuery = "INSERT INTO studio (kapasitas, harga_kursi) VALUE ('" _
+                                             & kapasitas & "', '" _
+                                             & harga_kursi & "')"
+            Debug.WriteLine(sqlQuery)
+
+            sqlCommand = New MySqlCommand(sqlQuery, dbConn)
+            sqlRead = sqlCommand.ExecuteReader
+            dbConn.Close()
+
+            sqlRead.Close()
+            dbConn.Close()
+        Catch ex As Exception
+            Return ex.Message
+        Finally
+            dbConn.Dispose()
+        End Try
+
+    End Function
+
+    Public Function GetDataStudioByIDDatabase(ID As Integer) As List(Of String)
+        Dim result As New List(Of String)
+
+        dbConn.ConnectionString = "server =" + server + ";" + "user id=" + username + ";" _
+        + "password=" + password + ";" + "database=" + database
+        dbConn.Open()
+
+        sqlCommand.Connection = dbConn
+        sqlCommand.CommandText = "SELECT id,
+                                    kapasitas,
+                                    harga_kursi
+                                    FROM studio
+                                    WHERE id='" & ID & "'"
+
+        sqlRead = sqlCommand.ExecuteReader
+
+        While sqlRead.Read
+            result.Add(sqlRead.GetString(0).ToString())
+            result.Add(sqlRead.GetString(1).ToString())
+            result.Add(sqlRead.GetString(2).ToString())
+        End While
+
+        sqlRead.Close()
+        dbConn.Close()
+        Return result
+    End Function
+
+    Public Function UpdateDataStudioByIDDatabase(ID As Integer,
+                                                 kapasitas As String,
+                                                 harga_kursi As String)
+        kapasitas = kapasitas.ToString()
+        harga_kursi = harga_kursi.ToString()
+
+        dbConn.ConnectionString = "server =" + server + ";" + "user id=" + username + ";" _
+        + "password=" + password + ";" + "database=" + database
+
+        Try
+            dbConn.Open()
+            sqlCommand.Connection = dbConn
+            sqlQuery = "UPDATE studio SET " &
+                        "kapasitas='" & kapasitas & "', " &
+                        "harga_kursi='" & harga_kursi & "' " &
+                        "WHERE id='" & ID & "'"
+
+            Debug.WriteLine(sqlQuery)
+            sqlCommand = New MySqlCommand(sqlQuery, dbConn)
+            sqlRead = sqlCommand.ExecuteReader
+
+            sqlRead.Close()
+            dbConn.Close()
+        Catch ex As Exception
+            Return ex.Message
+        Finally
+            dbConn.Dispose()
+        End Try
+
+        Return 0
+    End Function
+
+    Public Function DeleteDataStudioByIDDatabase(ID As Integer)
+        dbConn.ConnectionString = "server =" + server + ";" + "user id=" + username + ";" _
+        + "password=" + password + ";" + "database=" + database
+
+        Try
+            dbConn.Open()
+            sqlCommand.Connection = dbConn
+            sqlQuery = "DELETE FROM studio " &
+                        "WHERE id='" & ID & "'"
+
+            Debug.WriteLine(sqlQuery)
+
+            sqlCommand = New MySqlCommand(sqlQuery, dbConn)
+            sqlRead = sqlCommand.ExecuteReader
+            dbConn.Close()
+
+            sqlRead.Close()
+            dbConn.Close()
+        Catch ex As Exception
+            Return ex.Message
+        Finally
+            dbConn.Dispose()
+        End Try
+
+        Return 0
+    End Function
+End Class
